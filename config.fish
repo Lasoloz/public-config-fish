c0c375818902c3616be18611e69a0ab4b9b8b2a1
László Heim <lasolozdev@gmail.com>
2023-04-17 23:22:47 +0300

    Better fish colors for dark mode (cmd and params)

--- config.fish
+++ config.fish
@@ -3,6 +3,10 @@ if status is-interactive
 end
 
 
+# Configuring fish colors:
+set -g fish_color_command '#c397d8'
+set -g fish_color_param '#c5c8c6'
+
 # Configuring tide:
 set -g tide_prompt_add_newline_before true
 
@@ -14,6 +18,9 @@ set -g tide_right_prompt_items status cmd_duration jobs
 set -g tide_git_bg_color '#2e4028'
 set -g tide_git_bg_color_unstable '#2e4028' #'#403428'
 set -g tide_git_bg_color_urgent '#7d2d2d'
+
+set -g tide_pwd_color_truncated_dirs '#4cbcc2'
+set -g tide_pwd_color_dirs '#6dd8de'
 
 set -g tide_rustc_bg_color '#38020b'
 
